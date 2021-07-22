df1bc4899ef50465ba684036802aa65ad03511a2
Cindy Girardin-Spieser <cindy.girardin@live.fr>
2021-07-22 23:42:16 +0200

    first commit functions.py

---
+++ P7_01_functions.py
@@ -0,0 +1,306 @@
+'''Fichier contenant les fonctions du P7'''
+
+import numpy as np
+import pandas as pd
+import matplotlib.pyplot as plt
+import seaborn as sns
+from sklearn.metrics import roc_auc_score, precision_recall_curve, roc_curve, average_precision_score, confusion_matrix
+from lightgbm import LGBMClassifier
+
+def reduce_mem_usage(df):
+    """ Reduce Memory Usage :
+        iterate through all the columns of a dataframe and modify the data type
+        to reduce memory usage.        
+    """
+    start_mem = df.memory_usage().sum() / 1024**2
+    print('Memory usage of dataframe is {:.2f} MB'.format(start_mem))
+    
+    for col in df.columns:
+        col_type = df[col].dtype
+        
+        if col_type != object:
+            c_min = df[col].min()
+            c_max = df[col].max()
+            
+            if str(col_type)[:3] == 'int':
+                if c_min > np.iinfo(np.int8).min and c_max < np.iinfo(np.int8).max:
+                    df[col] = df[col].astype(np.int8)
+                elif c_min > np.iinfo(np.int16).min and c_max < np.iinfo(np.int16).max:
+                    df[col] = df[col].astype(np.int16)
+                elif c_min > np.iinfo(np.int32).min and c_max < np.iinfo(np.int32).max:
+                    df[col] = df[col].astype(np.int32)
+                elif c_min > np.iinfo(np.int64).min and c_max < np.iinfo(np.int64).max:
+                    df[col] = df[col].astype(np.int64)  
+            else:
+                if c_min > np.finfo(np.float16).min and c_max < np.finfo(np.float16).max:
+                    df[col] = df[col].astype(np.float16)
+                elif c_min > np.finfo(np.float32).min and c_max < np.finfo(np.float32).max:
+                    df[col] = df[col].astype(np.float32)
+                else:
+                    df[col] = df[col].astype(np.float64)
+        else:
+            df[col] = df[col].astype('category')
+
+    end_mem = df.memory_usage().sum() / 1024**2
+    print('Memory usage after optimization is: {:.2f} MB'.format(end_mem))
+    print('Decreased by {:.1f}%'.format(100 * (start_mem - end_mem) / start_mem))
+    
+    return df
+
+
+def one_hot_encoder(df, nan_as_category=True):
+    '''One-hot encoding for categorical columns with get_dummies'''
+    original_columns = list(df.columns)
+    categorical_columns = df.select_dtypes(["category", "object"]).columns.tolist()
+    # categorical_columns = [col for col in df.columns if df[col].dtype == 'object']
+    df = pd.get_dummies(df, columns=categorical_columns, dummy_na=nan_as_category)
+    new_columns = [c for c in df.columns if c not in original_columns]
+    return df, new_columns
+
+def grab_col_names(dataframe, cat_th=10, car_th=20, show_date=False):
+    '''Grab Column Names'''
+    date_cols = [col for col in dataframe.columns if dataframe[col].dtypes == "datetime64[ns]"]
+
+    #cat_cols = [col for col in dataframe.columns if dataframe[col].dtypes == "O"]
+    cat_cols = dataframe.select_dtypes(["object", "category"]).columns.tolist()
+    
+    
+    
+    num_but_cat = [col for col in dataframe.select_dtypes(["float", "integer"]).columns if dataframe[col].nunique() < cat_th]
+
+    cat_but_car = [col for col in dataframe.select_dtypes(["object", "category"]).columns if dataframe[col].nunique() > car_th]
+
+    cat_cols = cat_cols + num_but_cat
+    cat_cols = [col for col in cat_cols if col not in cat_but_car]
+
+    num_cols = dataframe.select_dtypes(["float", "integer"]).columns
+    num_cols = [col for col in num_cols if col not in num_but_cat]
+
+    print(f"Observations: {dataframe.shape[0]}")
+    print(f"Variables: {dataframe.shape[1]}")
+    print(f'date_cols: {len(date_cols)}')
+    print(f'cat_cols: {len(cat_cols)}')
+    print(f'num_cols: {len(num_cols)}')
+    print(f'cat_but_car: {len(cat_but_car)}')
+    print(f'num_but_cat: {len(num_but_cat)}')
+
+    # cat_cols + num_cols + cat_but_car = değişken sayısı.
+    # num_but_cat cat_cols'un içerisinde zaten.
+    # dolayısıyla tüm şu 3 liste ile tüm değişkenler seçilmiş olacaktır: cat_cols + num_cols + cat_but_car
+    # num_but_cat sadece raporlama için verilmiştir.
+
+    if show_date == True:
+        return date_cols, cat_cols, cat_but_car, num_cols, num_but_cat
+    else:
+        return cat_cols, cat_but_car, num_cols, num_but_cat
+
+def cat_analyzer(dataframe, variable, target = None):
+    '''Categorical Variables & Target'''
+    print(variable)
+    if target == None:
+        print(pd.DataFrame({
+            "COUNT": dataframe[variable].value_counts(),
+            "RATIO": dataframe[variable].value_counts() / len(dataframe)}), end="\n\n\n")
+    else:
+        temp = dataframe[dataframe[target].isnull() == False]
+        print(pd.DataFrame({
+            "COUNT":dataframe[variable].value_counts(),
+            "RATIO":dataframe[variable].value_counts() / len(dataframe),
+            "TARGET_COUNT":dataframe.groupby(variable)[target].count(),
+            "TARGET_MEAN":temp.groupby(variable)[target].mean(),
+            "TARGET_MEDIAN":temp.groupby(variable)[target].median(),
+            "TARGET_STD":temp.groupby(variable)[target].std()}), end="\n\n\n")
+
+def corr_plot(data, remove=["Id"], corr_coef = "pearson", figsize=(20, 20)):
+    '''Numerical Variables'''
+    if len(remove) > 0:
+        num_cols2 = [x for x in data.columns if (x not in remove)]
+
+    sns.set(font_scale=1.1)
+    c = data[num_cols2].corr(method = corr_coef)
+    mask = np.triu(c.corr(method = corr_coef))
+    plt.figure(figsize=figsize)
+    sns.heatmap(c,
+                annot=True,
+                fmt='.1f',
+                cmap='coolwarm',
+                square=True,
+                mask=mask,
+                linewidths=1,
+                cbar=False)
+    plt.show()
+
+def num_plot(data, num_cols, remove=["Id"], hist_bins=10, figsize=(20, 4)):
+    '''Plot numerical variables'''
+
+    if len(remove) > 0:
+        num_cols2 = [x for x in num_cols if (x not in remove)]
+
+    for i in num_cols2:
+        fig, axes = plt.subplots(1, 3, figsize=figsize)
+        data.hist(str(i), bins=hist_bins, ax=axes[0])
+        data.boxplot(str(i), ax=axes[1], vert=False);
+        try:
+            sns.kdeplot(np.array(data[str(i)]))
+        except:
+            ValueError
+
+        axes[1].set_yticklabels([])
+        axes[1].set_yticks([])
+        axes[0].set_title(i + " | Histogram")
+        axes[1].set_title(i + " | Boxplot")
+        axes[2].set_title(i + " | Density")
+        plt.show()
+
+def high_correlation(data, remove=['SK_ID_CURR', 'SK_ID_BUREAU'], corr_coef="pearson", corr_value = 0.7):
+    '''Get high correlated variables'''
+    if len(remove) > 0:
+        cols = [x for x in data.columns if (x not in remove)]
+        c = data[cols].corr(method=corr_coef)
+    else:
+        c = data.corr(method=corr_coef)
+
+    for i in c.columns:
+        cr = c.loc[i].loc[(c.loc[i] >= corr_value) | (c.loc[i] <= -corr_value)].drop(i)
+        if len(cr) > 0:
+            print(i)
+            print("-------------------------------")
+            print(cr.sort_values(ascending=False))
+            print("\n")
+
+def missing_values(data, plot=False):
+    '''Missing Value'''
+    mst = pd.DataFrame(
+        {"Num_Missing": data.isnull().sum(), "Missing_Ratio": data.isnull().sum() / data.shape[0]}).sort_values(
+        "Num_Missing", ascending=False)
+    mst["DataTypes"] = data[mst.index].dtypes.values
+    mst = mst[mst.Num_Missing > 0].reset_index().rename({"index": "Feature"}, axis=1)
+
+    print("Number of Variables include Missing Values:", mst.shape[0], "\n")
+
+    if mst[mst.Missing_Ratio >= 1.0].shape[0] > 0:
+        print("Full Missing Variables:", mst[mst.Missing_Ratio >= 1.0].Feature.tolist())
+        data.drop(mst[mst.Missing_Ratio >= 1.0].Feature.tolist(), axis=1, inplace=True)
+
+        print("Full missing variables are deleted!", "\n")
+
+    if plot:
+        plt.figure(figsize=(25, 8))
+        p = sns.barplot(mst.Feature, mst.Missing_Ratio)
+        for rotate in p.get_xticklabels():
+            rotate.set_rotation(90)
+        plt.show()
+
+    print(mst, "\n")
+    
+
+def quantile_funcs(percentiles = [0.75, 0.9, 0.99]):
+    '''Quantile functions for aggregations'''
+    return [(p, lambda x: x.quantile(p)) for p in percentiles]
+
+def rare_encoder(data, col, rare_perc):
+    temp = data[col].value_counts() / len(data) < rare_perc
+    data[col] = np.where(~data[col].isin(temp[temp < rare_perc].index), "Rare", data[col])
+    
+def plt_confusion_matrix(y_true, y_pred):
+    '''fonction qui trace la matrice de confusion'''
+    plt.figure(figsize=(10,6))
+    sns.heatmap(confusion_matrix(y_true, y_pred), annot=True, cmap="YlGnBu")
+    plt.ylabel('True classes', fontsize=14)
+    plt.xlabel('Predicted classes', fontsize=14)
+    plt.title('Matrice de confusion', fontsize=20)
+    print(classification_report(y_true, y_pred))
+
+def display_importances(feature_importance_df_):
+    # Plot feature importances
+    cols = feature_importance_df_[["feature", "importance"]].groupby(
+        "feature").mean().sort_values(
+            by="importance", ascending=False)[:50].index
+
+    best_features = feature_importance_df_.loc[
+        feature_importance_df_.feature.isin(cols)]
+
+    plt.figure(figsize=(8, 10))
+    sns.barplot(
+        x="importance",
+        y="feature",
+        data=best_features.sort_values(by="importance", ascending=False))
+    plt.title('LightGBM Features (avg over folds)')
+    plt.tight_layout()
+    plt.savefig('lgbm_importances-01.png')
+
+def display_roc_curve(y_, oof_preds_, folds_idx_):
+    # Plot ROC curves
+    plt.figure(figsize=(6, 6))
+    scores = []
+    for n_fold, (_, val_idx) in enumerate(folds_idx_):
+        # Plot the roc curve
+        fpr, tpr, thresholds = roc_curve(y_.iloc[val_idx], oof_preds_[val_idx])
+        score = roc_auc_score(y_.iloc[val_idx], oof_preds_[val_idx])
+        scores.append(score)
+        plt.plot(
+            fpr,
+            tpr,
+            lw=1,
+            alpha=0.3,
+            label='ROC fold %d (AUC = %0.4f)' % (n_fold + 1, score))
+
+    plt.plot(
+        [0, 1], [0, 1],
+        linestyle='--',
+        lw=2,
+        color='r',
+        label='Luck',
+        alpha=.8)
+    fpr, tpr, thresholds = roc_curve(y_, oof_preds_)
+    score = roc_auc_score(y_, oof_preds_)
+    plt.plot(
+        fpr,
+        tpr,
+        color='b',
+        label='Avg ROC (AUC = %0.4f $\pm$ %0.4f)' % (score, np.std(scores)),
+        lw=2,
+        alpha=.8)
+
+    plt.xlim([-0.05, 1.05])
+    plt.ylim([-0.05, 1.05])
+    plt.xlabel('False Positive Rate')
+    plt.ylabel('True Positive Rate')
+    plt.title('LightGBM ROC Curve')
+    plt.legend(loc="lower right")
+    plt.tight_layout()
+
+def display_precision_recall(y_, oof_preds_, folds_idx_):
+    # Plot ROC curves
+    plt.figure(figsize=(6, 6))
+
+    scores = []
+    for n_fold, (_, val_idx) in enumerate(folds_idx_):
+        # Plot the roc curve
+        fpr, tpr, thresholds = roc_curve(y_.iloc[val_idx], oof_preds_[val_idx])
+        score = average_precision_score(y_.iloc[val_idx], oof_preds_[val_idx])
+        scores.append(score)
+        plt.plot(
+            fpr,
+            tpr,
+            lw=1,
+            alpha=0.3,
+            label='AP fold %d (AUC = %0.4f)' % (n_fold + 1, score))
+
+    precision, recall, thresholds = precision_recall_curve(y_, oof_preds_)
+    score = average_precision_score(y_, oof_preds_)
+    plt.plot(
+        precision,
+        recall,
+        color='b',
+        label='Avg ROC (AUC = %0.4f $\pm$ %0.4f)' % (score, np.std(scores)),
+        lw=2,
+        alpha=.8)
+
+    plt.xlim([-0.05, 1.05])
+    plt.ylim([-0.05, 1.05])
+    plt.xlabel('Recall')
+    plt.ylabel('Precision')
+    plt.title('LightGBM Recall / Precision')
+    plt.legend(loc="best")
+    plt.tight_layout()
