aeed21c488e322204361c5d82eb0ab36a5c46920
Maxime Haegeman <74610219+mhaegeman@users.noreply.github.com>
2022-03-12 12:54:42 +0100

    type slider change

--- dashboard_app_streamlit.py
+++ dashboard_app_streamlit.py
@@ -288,7 +288,7 @@ with st.spinner('Calcul en cours'):
             st.write("## Prédiction",
                      results["Probabilité d'impayés"])
 
-        seuil = st.slider("Choisissez le seuil d'acceptation", min_value=0, max_value=1, value=0.5, step=0.01)
+        seuil = st.slider("Choisissez le seuil d'acceptation", min_value=0.00, max_value=1.00, value=0.50, step=0.01)
 
         proba = results["Probabilité d'impayés"].iloc[0]
         def_p = "Le client a déjà été en défaut de paiement : " + str(df_client_int['Défaut paiement'].iloc[0])
